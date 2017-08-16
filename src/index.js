ae1fff1b709f861ecbb54354610ebee291577b77
Fliss Hou <flissh@arisan.io>
2017-08-16 11:56:31 +0800

    Add index file

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://io-arisan-clio-sandbox:IOX-42646663-iox@ds054639-a1.mlab.com:54632,ds054639-a0.mlab.com:54639/io-arisan-clio-sandbox?replicaSet=rs-ds054639');
